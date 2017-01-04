b93f816c2d9d86b1f59a6f9ecfc73e981fe94791
Cleophas Kalekem <kalekemcleophas01@gmail.com>
2017-01-04 15:04:13 -0800

    Added projects to the website

--- thankyou.php
+++ thankyou.php
@@ -2,8 +2,6 @@
  
 require 'connection.php';
 
-
-*/
 $conn = Connect();
 $your_name = $conn->real_escape_string($_POST['your_name']);  
 $your_email = $conn->real_escape_string($_POST['your_email']);
